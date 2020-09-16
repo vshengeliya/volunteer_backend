055af2ca505f9a71240e94df22eca46d2a25a9ec
Veronika Dodda <v.shengeliya@gmail.com>
2020-09-16 16:12:58 -0400

    added first_name to serializers

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :last_name, :password_digest, :email, :image, :city, :state, :events, :my_attendances
+  attributes :id, :first_name, :last_name, :password_digest, :email, :image, :city, :state, :events, :my_attendances
 
 end
