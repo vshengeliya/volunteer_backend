05e213e311a466e27af222d8c5e106a57e5b05f7
Veronika Dodda <v.shengeliya@gmail.com>
2020-09-21 16:41:44 -0400

    update user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,7 +4,36 @@ class Api::V1::UsersController < ApplicationController
         @users = User.all
         render json: @users
     end
+
+    def show
+        user = User.find(params[:id])
+        render json: user
+      end
     
     def create
+    end
+
+    def edit
+        user = User.find(params[:id])
+        render json: user
+      end
+
+    def update
+        user = User.find(params[:id])
+        user.update(user_params)
+        render json: user
+    end
+    
+    def destroy
+        user = User.find(params[:id])
+        user.destroy!
+        render json: {}
+    end
+    
+    
+    private
+    
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :image, :city, :state, :email, :password_digest)
     end
 end
